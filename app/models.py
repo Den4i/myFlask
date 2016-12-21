b4cea0b89225bb7b6ecc0ae81e5f34ae6c852bc1
Den4i <dglonassik@gmail.com>
2016-12-22 00:43:41 +0300

    add model User

---
+++ app/models.py
@@ -0,0 +1,25 @@
+from app import db
+
+
+class User(db.Model):
+    id_ = db.Column(db.Integer, primary_key=True)
+    name_ = db.Column(db.String(50))
+    pass_ = db.Column(db.String(50))
+    fact_addr_lon = db.Column(db.String(25), nullable=True)
+    fact_addr_lat = db.Column(db.String(25), nullable=True)
+
+    __tablename__ = 'users'
+
+    def is_active(self):
+        return True
+
+    def get_id(self):
+        # returns the user e-mail. not sure who calls this
+        return self.email
+
+    def is_authenticated(self):
+        return self.authenticated
+
+    def is_anonymous(self):
+        # False as we do not support annonymity
+        return False
