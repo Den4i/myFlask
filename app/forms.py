0d0509b9b386ffa82d2e63e93df5f40b7b5fe192
Den4i <dglonassik@gmail.com>
2016-12-22 00:42:34 +0300

    ref loginform 2 name|pass field

--- app/forms.py
+++ app/forms.py
@@ -1,8 +1,11 @@
 from flask_wtf import FlaskForm
-from wtforms import StringField, BooleanField
+from wtforms import StringField
 from wtforms.validators import DataRequired
 
 
 class LoginForm(FlaskForm):
-    openid = StringField('openid', validators=[DataRequired()])
-    remember_me = BooleanField('remember_me', default=False)
+    name_ = StringField('name_', validators=[DataRequired()])
+    pass_ = StringField('pass_', validators=[DataRequired()])
+
+
+
