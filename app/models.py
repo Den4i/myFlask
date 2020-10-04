d9ce78e3a3d10f96228bdd38b5b7be507673be5e
Денис Папилин <d.papilin@litebox.ru>
2020-10-04 21:56:57 +0400

    Удаление неиспользуемых Моделей

--- app/models.py
+++ app/models.py
@@ -1,6 +1,6 @@
-from sqlalchemy import Column, String, Integer, Sequence, ForeignKey
+from sqlalchemy import Column, String, Integer, ForeignKey
 
-from sqlalchemy.orm import relationship, backref
+from sqlalchemy.orm import relationship
 from sqlalchemy.ext.declarative import declarative_base
 
 Base = declarative_base()
@@ -46,27 +46,3 @@ class Object(Base):
     project = relationship(Project)
     route = relationship(Route)
     provider = relationship(Provider)
-
-
-class Person(Base):
-    __tablename__ = 'persons'
-    id = Column(Integer, Sequence('person_id_seq'), primary_key=True)
-    name = Column(String(50))
-
-    address_id = Column(Integer, ForeignKey('address.id'))
-    address = relationship('Address', backref=backref('persons', lazy='dynamic'))
-
-    def __init__(self, name):
-        self.name = name
-
-    def __repr__(self):
-        return "Person(id='%s', name='%s')" % (self.id, self.name)
-
-
-class Address(Base):
-    __tablename__ = 'address'
-    id = Column(Integer, Sequence('address_id_seq'), primary_key=True)
-    email = Column(String(50))
-
-    def __init__(self, email):
-        self.email = email
